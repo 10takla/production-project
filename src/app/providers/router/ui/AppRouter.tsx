331baeb35c427940f295792485301b04476fab8b
10takla <letifovabakar98@gmail.com>
2023-07-03 00:11:25 +0300

    add babel, add extract plugin

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,23 @@
+import React, {Suspense} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {routeConfig} from "shared/config/routerConfig/routeConfig";
+
+const AppRouter = () => {
+    return (
+        <Suspense fallback={<>Загрузка</>}>
+            <Routes>
+                {Object.values(routeConfig).map(({path, element}) => (
+                    <Route key={path} {...{path,}} element={
+                        <div className={'page-wrapper'}>
+                            {element}
+                        </div>
+                    }/>
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
+
+export default AppRouter;
