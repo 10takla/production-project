87ba54b51a207c8e878ade1dbce9b42eae217b93
10takla <letifovabakar98@gmail.com>
2023-07-06 00:49:44 +0300

    add jest, add first test

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { classNames } from 'shared/lib/className/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.mainLink}>
-            <AppLink theme={AppLinkTheme.PRIMARY} to="/">Главная страница</AppLink>
-            <AppLink theme={AppLinkTheme.PRIMARY} to="/about">о сайте</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.mainLink}>
+                <AppLink theme={AppLinkTheme.PRIMARY} to="/">
+                    {t('Главная страница')}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} to="/about">
+                    {t('о сайте')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwithcer';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -14,9 +15,16 @@ const Sidebar = ({ className }: SidebarProps) => {
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
+    const { t } = useTranslation();
+
     return (
-        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <Button onClick={onToggle}>Toggle</Button>
+        <div className={classNames(
+            cls.Sidebar,
+            { [cls.collapsed]: collapsed },
+            [className],
+        )}
+        >
+            <Button onClick={onToggle}>{(t('Toggle'))}</Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
