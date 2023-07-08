522561ee96c4a07b4f38db5b8e5811ae12400c3c
10takla <letifovabakar98@gmail.com>
2023-07-09 01:22:31 +0300

    add main pipeline github actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,10 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack, {DefinePlugin} from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import {BuildPaths} from './types/config';
+import {BuildOptions} from './types/config';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
-export function buildPlugins(paths: BuildPaths, isDev: boolean): webpack.WebpackPluginInstance[] {
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -14,21 +14,17 @@ export function buildPlugins(paths: BuildPaths, isDev: boolean): webpack.Webpack
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new DefinePlugin({
+        new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: true
-        }),
     ];
+
     if (isDev) {
-        plugins.push(
-            new webpack.HotModuleReplacementPlugin(),
-            new BundleAnalyzerPlugin({
-                openAnalyzer: true
-            })
-        )
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
 
-    return plugins
+    return plugins;
 }
