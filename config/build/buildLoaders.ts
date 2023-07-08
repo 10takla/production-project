e3e0584dfb1c56548c53969f22911c1bdae4bb44
10takla <letifovabakar98@gmail.com>
2023-07-09 01:32:30 +0300

    add main pipeline github actions

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
-import {buildCssLoaders} from  './loaders/buildCssLoaders'
+import { buildCssLoaders } from './loaders/buildCssLoaders';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const typescriptLoader = {
@@ -41,8 +41,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-
-    const cssLoaders = buildCssLoaders(isDev)
+    const cssLoaders = buildCssLoaders(isDev);
     return [
         fileLoader,
         svgLoader,
